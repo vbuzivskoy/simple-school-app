6efbe7e3dbf80296860adee7434a53dbb365c6bc
Buzivskoy Vyacheslav <vyacheslav.buzivskoy@ukmig.com.ua>
2021-01-13 22:04:48 +0200

    feat: Add GET teachers route

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,20 @@
 import { Pool } from 'pg';
-import Express from 'express';
+import Express, { Request, Response } from 'express';
 
 import { configureApp, AppConfig } from './config';
-import { initDatabase } from './db';
+import { initDatabase, getAllTeachers } from './db';
+import { Teacher } from './models';
 
 const appConfig: AppConfig = configureApp();
 const pool: Pool = initDatabase(appConfig.db);
 
 const app = Express();
+
+app.get('/teachers', (req: Request, res: Response) => {
+  getAllTeachers(pool)
+    .then((teachers: Teacher[]) => res.status(200).json(teachers))
+    .catch((e) => res.status(500).json('Internal Server error'));
+});
 
 app.listen(appConfig.app.port, () => {
   console.log(`App listening on port ${appConfig.app.port}`);
