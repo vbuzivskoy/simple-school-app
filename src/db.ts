bce3c2a51e9862012a9b06c476b63b28c5604b29
Buzivskoy Vyacheslav <vyacheslav.buzivskoy@ukmig.com.ua>
2021-01-13 16:51:23 +0200

    feat: Add teacher read from db logic

--- src/db.ts
+++ src/db.ts
@@ -1,6 +1,30 @@
-import { Pool, ClientConfig } from 'pg';
+import { Pool, ClientConfig, QueryResult } from 'pg';
+
+import { Teacher } from './models';
 
 export const initDatabase = (dbConfig: ClientConfig): Pool => {
   const pool = new Pool(dbConfig);
   return pool;
+};
+
+export const getAllTeachers = async (pool: Pool): Promise<Teacher[]> => {
+  const query: string = 'SELECT * FROM teachers;';
+  const result: QueryResult = await pool.query(query);
+  return result.rows;
+};
+
+export const getTargetMathTeachers = async (pool: Pool): Promise<Teacher[]> => {
+  const query: string = `SELECT teachers.* FROM lessons
+    JOIN teachers ON lessons.teacher_id = teachers.id
+    JOIN teachers_to_subjects ON teachers_to_subjects.teacher_id = teachers.id
+    JOIN subjects ON teachers_to_subjects.subject_id = subjects.id
+    JOIN classrooms ON lessons.classroom_id = classrooms.id
+    WHERE classrooms.classroom_number = 100
+      AND subjects.title = 'Math'
+      AND years_of_experience > 10
+      AND day_of_week = 'Thursday'
+      AND start_time BETWEEN '08:30' AND '14:30'
+    ORDER BY id ASC;`;
+  const result: QueryResult = await pool.query(query);
+  return result.rows;
 };
