1165b1b3accd44c0f6a55bcf92596d836653aecb
Buzivskoy Vyacheslav <vyacheslav.buzivskoy@ukmig.com.ua>
2021-01-14 13:18:47 +0200

    style: Format model.ts

--- src/models.ts
+++ src/models.ts
@@ -1,33 +1,49 @@
-enum sex {
+import { studentGroupId } from './const';
+
+enum sex_types {
   Male,
   Female,
 }
 
-enum subject {
-  Biology,
-  Math,
-  Physics,
-  Chemistry,
+enum days_of_week {
+  Monday,
+  Tuesday,
+  Wednesday,
+  Thursday,
+  Friday,
+  Saturday,
+  Sunday,
 }
 
-interface Teacher {
-  id: string;
+export interface subject {
+  id: number;
+  title: string;
+}
+
+export interface Teacher {
+  id?: number;
   fullName: string;
   age: number;
-  sex: sex;
+  sex: sex_types;
   yearsOfExperience: number;
-  canTeachSubjects: subject[];
 }
 
-interface Lesson {
-  id: string;
-  subject: subject;
-  teacherId: string;
-  classroomId: string;
-  datetime: Date;
-}
-
-interface Classroom {
-  id: string;
+export interface Classroom {
+  id: number;
   classroomNumber: string;
+}
+
+export interface StudentGroup {
+  id: number;
+  title: string;
+}
+
+export interface Lesson {
+  id: number;
+  subjectId: number;
+  teacherId: number;
+  studentsGroupId: number;
+  classroomId: number;
+  dayOfWeek: days_of_week;
+  time: Date;
 }
