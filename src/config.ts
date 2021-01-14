3237d9975e4cd779ce0e005986abc37ec532995f
Buzivskoy Vyacheslav <vyacheslav.buzivskoy@ukmig.com.ua>
2021-01-14 13:18:09 +0200

    feat: Add application port config parametr to config.ts

--- src/config.ts
+++ src/config.ts
@@ -3,6 +3,7 @@ import { ClientConfig } from 'pg';
 
 export interface AppConfig {
   db: ClientConfig;
+  app: { port: number };
 }
 
 export const configureApp = (): AppConfig => {
@@ -20,5 +21,6 @@ export const configureApp = (): AppConfig => {
       database: process.env.DB_NAME || 'postgres',
       port: parseInt(process.env.DB_PORT || '5432'),
     },
+    app: { port: parseInt(process.env.APP_PORT || '3000') },
   };
 };
